dd2abdfd61ad316724670d1be142ba13b0210a07
gobind morya <govindmaurya8999@gmail.com>
2023-12-03 07:37:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+
+import './App.css';
+import React,{useRef} from 'react';
+import * as tf from "@tensorflow/tfjs"
+import * as handpose from "@tensorflow-models/handpose"
+import Webcam from "react-webcam"
+import { drawhand } from './utilities';
+
+function App() {
+  const webcamRef = useRef(null) 
+  const canvasRef = useRef(null)
+  const runHandpose = async () => {
+    const net = await handpose.load()
+    console.log("handpose model loaded")
+    setInterval(()=>
+    {
+      detect(net)
+    },100)
+  }
+  const detect = async(net) =>{
+    if(typeof webcamRef.current !== 'undefined' && webcamRef.current !== null && webcamRef.current.video.readyState === 4){
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+        webcamRef.current.video.width = videoWidth ;
+        webcamRef.current.video.height = videoHeight ;
+        canvasRef.current.width = videoWidth ;
+        canvasRef.current.height = videoHeight ;
+
+        const hand = await net.estimateHands(video);
+        console.log(hand);
+      
+        const ctx = canvasRef.current.getContext("2d");
+        drawhand(hand,ctx);
+
+    }
+  }
+  runHandpose();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+        ref={webcamRef} 
+        style={{
+          position:'absolute',
+          marginRight:'auto',
+          marginLeft:'auto',
+          left:0,
+          right:0,
+          textAlign:'center',
+          zIndex:9,
+          width:640,
+          height:480
+        }}
+        />
+        <canvas 
+        ref={canvasRef}
+        style={{
+          position:'absolute',
+          marginRight:'auto',
+          marginLeft:'auto',
+          left:0,
+          right:0,
+          textAlign:'center',
+          zIndex:9,
+          width:640,
+          height:480,
+        }}
+      
+        ></canvas>
+      </header>
+    </div>
+  );
+}
+
+export default App;
